b941b851e50771dd9900dcdfe7c0e8167d6dc50e
alexandre-daumail <alexandre.daumail@laplateforme.io>
2022-11-30 15:54:35 +0100

    added profiler

--- api-symfony/src/Entity/User.php
+++ api-symfony/src/Entity/User.php
@@ -140,22 +140,8 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         return $this->createdAt;
     }
 
-    public function setCreatedAt(\DateTimeImmutable $createdAt): self
-    {
-        $this->createdAt = $createdAt;
-
-        return $this;
-    }
-
     public function getUpdatedAt(): ?\DateTimeImmutable
     {
         return $this->updatedAt;
-    }
-
-    public function setUpdatedAt(?\DateTimeImmutable $updatedAt): self
-    {
-        $this->updatedAt = $updatedAt;
-
-        return $this;
     }
 }
